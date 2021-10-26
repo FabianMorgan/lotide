92cdfbe92683b954ec9de4cde3222e5e77b19121
FabianMorgan <fabagan@yahoo.com>
2021-10-26 19:47:03 +0000

    learning function head

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) { 
+  if (actual === expected) {
+    return console.log(`Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    return console.log(`Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+function head (arr) {
+  if(arr.length) 
+   return arr[0]
+}
+// TEST CODE
+assertEqual('light', 'labs');
+assertEqual(2, 3);
+
+//arr[arr.length - 1] - last index of array
