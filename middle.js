02f6d6471408619c16b3b3eba24079bf85354807
Fabian Morgan <fabagan@yahoo.com>
2021-11-01 18:45:55 -0400

    refactoring middle.js

--- middle.js
+++ middle.js
@@ -2,29 +2,6 @@
 // 2. test if type of elements in both arrays are equal
 // 3. test if value of each item in both arrays are equal
 
-const eqArrays = function(arr1, arr2) {
-  
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let val1 of arr1) {
-    if (arr1[val1] !== arr2[val1]) {
-      return false;
-
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`Assertion failed:  ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   //returning [] for arrays with 1 or 2 elements.
   let endArray = [];
@@ -39,12 +16,6 @@ const middle = function(array) {
   }
 };
 
+module.exports = middle;
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
