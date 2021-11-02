3617e0fc9aa7a2e00a04c9907c24d597942ac844
Fabian Morgan <fabagan@yahoo.com>
2021-11-02 13:43:52 -0400

    Using Mocha and Chai for testing

--- test/head.Test.js
+++ test/head.Test.js
@@ -1,7 +1,20 @@
 // test/head.js
+
+const assert = require('chai').assert;
 const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3, 4]", () => {
+    assert.strictEqual(head([1, 2, 3, 4]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5');
+});
 
 // TEST CODE
 console.log(head(['light', 'labs']));
 console.log(head([2, 3]));
 console.log(head([2, 3, 4]));
+
