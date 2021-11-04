35d5c9a1f2f067c41c84a9a6084944836a74ca46
Fabian Morgan <fabagan@yahoo.com>
2021-11-04 15:05:31 -0400

    testing functions that return objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,16 @@
+const eqObjects = require("./eqObjects");
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅✅✅ Assertion passed: ${inspect(actual)} === {inspect(expected)}`
+    );
+  } else {
+    console.log(
+      `❌❌❌ Assertion failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+
+module.exports = assertObjectsEqual;
