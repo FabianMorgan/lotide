a8e4e30b622059697fc8786199b338ec19fcb8f4
Fabian Morgan <fabagan@yahoo.com>
2021-11-06 20:02:06 -0400

    map.js

---
+++ map.js
@@ -0,0 +1,18 @@
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+
+const words = ["love", "joy", "peace", "patience", "faithfulness", "kindness", "goodness", "gentleness", "self-control"];
+
+const map = function(array, callback) {  
+  const results = [];
+  
+  for (let item of array) {
+    results.push(callback(item));
+    console.log(results)
+  }  
+  return results;
+}
+
+module.exports = map;
